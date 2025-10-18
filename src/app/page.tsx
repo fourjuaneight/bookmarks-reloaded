7a79d5a26e4a36ff7d57a08ec1236c06756b7cab
Juan Villela <fourjuaneight@users.noreply.github.com>
2025-10-18 18:21:58 -0400

    chore: Add initial page.

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h1
+        className="col-start-2 font-bold mb-8 mt-0 text-5xl w-full sm:text-5xl"
+        data-line="slim"
+      >
+        Bookmarks (Reloaded)
+      </h1>
+    </>
+  );
+}
